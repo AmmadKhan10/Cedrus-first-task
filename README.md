fd79c5555f6171718f37c4ad3409c94f0f4fdc8f
Ammad khan <ak5321515@gmail.com>
2024-05-27 19:00:06 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cedrus-first-task
+ Simple React file with Personal Information
---
+++ first task/src/Profile.jsx
@@ -0,0 +1,14 @@
+function Profile() {
+    return(
+        <div className="parent1">
+        <h1>Personal introduction</h1>
+        <p>My name is AMMAD KHAN</p>
+        <p>My father name is Muhammad Ilyas Khan.</p>
+        <p>I am from Oghi, Mansehra.</p>
+        <p>My date of birth is 27/05/2000.</p>
+           
+        </div>
+    )
+}
+
+export default Profile;
